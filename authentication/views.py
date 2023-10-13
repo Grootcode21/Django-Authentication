6da63818006894a99492cdcb595e05625eb54fb9
Grootcode21 <antonyndichu15@gmail.com>
2023-10-12 22:09:50 -0700

    force_text into force_str

--- authentication/views.py
+++ authentication/views.py
@@ -9,7 +9,7 @@ from django.core.mail import send_mail
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from . tokens import generate_token
 
 
